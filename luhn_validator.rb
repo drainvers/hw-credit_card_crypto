4316aa342efc17ee531f1daadc3cadc3f2367fff
Daniel Kong <danielkong24@gmail.com>
2022-03-07 19:54:41 +0700

    First commit: implemented validator and class serialization/deserialization; validator tests passed; added bundler-audit to Gemfile

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Implementation of the Luhn algorithm
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
@@ -7,5 +10,17 @@ module LuhnValidator
     nums_a = number.to_s.chars.map(&:to_i)
 
     # TODO: use the integers in nums_a to validate its last check digit
+    check = nums_a
+            .reverse
+            .each_with_index
+            .reduce(0) do |a, (d, i)|
+      a + get_val(d, i)
+    end
+
+    check % 10 == 0
+  end
+
+  def get_val(digit, index)
+    (digit * (index.odd? ? 2 : 1)).digits.sum
   end
 end
